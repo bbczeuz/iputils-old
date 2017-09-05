20c9e7b11d304a60807af5db330d92b75b0bb36f
Claudius Zingerli <gitmail@zeuz.ch>
2017-09-05 02:52:50 +0200

    Ping: Fix rounding for time values

--- ping_common.c
+++ ping_common.c
@@ -794,13 +794,13 @@ restamp:
 		}
 		if (timing) {
 			if (triptime >= 100000)
-				printf(" time=%ld ms", triptime/1000);
+				printf(" time=%ld ms", (triptime+500)/1000);
 			else if (triptime >= 10000)
 				printf(" time=%ld.%01ld ms", triptime/1000,
-				       (triptime%1000)/100);
+				       ((triptime%1000)+50)/100);
 			else if (triptime >= 1000)
 				printf(" time=%ld.%02ld ms", triptime/1000,
-				       (triptime%1000)/10);
+				       ((triptime%1000)+5)/10);
 			else
 				printf(" time=%ld.%03ld ms", triptime/1000,
 				       triptime%1000);
